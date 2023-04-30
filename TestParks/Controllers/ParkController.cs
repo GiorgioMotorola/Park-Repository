4bb070f59ca0346f8d7da9c4de162fdf6c781fce
Mike Weatherford <mikesweatherford@gmail.com>
2023-04-29 20:33:00 -0400

    init push

---
+++ TestParks/Controllers/ParkController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TestParks.Models;
+
+namespace TestParks.Controllers
+{
+    public class ParkController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            
+            List<Park> parks = RetrieveNationalParks();
+
+            return View(parks);
+        }
+
+        private List<Park> RetrieveNationalParks()
+        {
+            string jsonUrl = "https://raw.githubusercontent.com/GiorgioMotorola/ParksJson/main/json";
+
+            using (HttpClient client = new HttpClient())
+            {
+                string jsonData = client.GetStringAsync(jsonUrl).GetAwaiter().GetResult();
+                var parks = JsonConvert.DeserializeObject<List<Park>>(jsonData);
+
+                return parks;
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            
+            List<Park> parks = RetrieveNationalParks();
+
+            
+                Park park = parks.FirstOrDefault(p => p.Id == id);
+
+            if (park == null)
+            {
+                return NotFound();
+            }
+
+            return View(park);
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TestParks/Models/Park.cs
@@ -0,0 +1,15 @@
+﻿namespace TestParks.Models
+{
+    public class Park
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? ParkMap { get; set; }
+        public string? State { get; set; }
+        public string? Coordinates { get; set; }
+        public string? DateEstAsPark { get; set; }
+        public string? Area { get; set; }
+        public string? Description { get; set; }
+    }
+}

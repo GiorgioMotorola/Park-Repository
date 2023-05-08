ff2c69c94138cfda52551c0ac6783dcb97c9f53c
Mike Weatherford <mikesweatherford@gmail.com>
2023-05-07 20:32:58 -0400

    add more propteries and re tooled the details page

--- TestParks/Models/Park.cs
+++ TestParks/Models/Park.cs
@@ -4,12 +4,16 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public string? ImageUrl { get; set; }
+        public string? StateIcon { get; set; }
+        public string? ImageUrlOne { get; set; }
+        public string? ImageUrlTwo { get; set; }
+        public string? ImageUrlThree { get; set; }
         public string? ParkMap { get; set; }
         public string? State { get; set; }
         public string? Coordinates { get; set; }
         public string? DateEstAsPark { get; set; }
         public string? Area { get; set; }
         public string? Description { get; set; }
+        public string? Geography { get; set; }
     }
 }
